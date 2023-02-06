b06cdded42c244ee38accf44a59719e024ca7b59
Skyler E. Bothe <120291635+Stone6142@users.noreply.github.com>
2023-02-06 10:44:34 -0600

    trying to fix after win/lose goes back to chess

--- onlinegames/multiplayerchess/public/js/app.js
+++ onlinegames/multiplayerchess/public/js/app.js
@@ -36,15 +36,15 @@ function onDragStart2(source, piece, position, orientation) {
   if (game.game_over()) {
     if (game.in_draw()) {
       alert('Game Draw!!');
-      location.reload();
+      window.location.reload();
     }
     else if (game.in_checkmate())
       if (turnt === 1) {
         alert('You won the game!!');
-        location.reload();
+        window.location.reload();
       } else {
         alert('You lost!!');
-        location.reload();
+        window.location.reload();
       }
     return false
   }
