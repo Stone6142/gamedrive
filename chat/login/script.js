557121d932fcfe7249e5df15ca31a32f79128bf9
Skyler E. Bothe <120291635+Stone6142@users.noreply.github.com>
2023-02-07 14:25:59 -0600

    Update script.js

--- chat/login/script.js
+++ chat/login/script.js
@@ -3,7 +3,7 @@ var attempt = 3; // Variable to count number of attempts.
 function validate() {
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
-  if (username == "Skyler" && password == ${{ mypass.PASS }}) {
+  if (username == "Skyler" && password == ${{ secrets.PASS }}) {
     alert("Login successfully");
     window.location = "../logins1/me75f1f49c-0211-4337-a364-b7602ec38ea3/"; // Redirecting to other page.
     return false;
