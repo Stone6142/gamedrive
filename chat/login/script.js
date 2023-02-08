23cef85f073eeb2f4a188bc0aae97f7da7b8dfdb
Skyler E. Bothe <120291635+Stone6142@users.noreply.github.com>
2023-02-08 09:17:45 -0600

    Update script.js

--- chat/login/script.js
+++ chat/login/script.js
@@ -1,10 +1,10 @@
-
+require('dotenv').config();
 var attempt = 3; // Variable to count number of attempts.
 // Below function Executes on click of login button.
 function validate() {
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
-  if (username == "Skyler" && password == APIKey) {
+  if (username == "Skyler" && password == ${process.env.SB_PASS}) {
     alert("Login successfully");
     window.location = "../logins1/me75f1f49c-0211-4337-a364-b7602ec38ea3/"; // Redirecting to other page.
     return false;
