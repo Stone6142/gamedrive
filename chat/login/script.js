da3d94f9a524e1ae457eca69fb496eeb5f2b0cbb
Sky B <120291635+Stone6142@users.noreply.github.com>
2023-03-10 15:01:47 -0600

    Update script.js

--- chat/login/script.js
+++ chat/login/script.js
@@ -10,15 +10,15 @@ function validate() {
     alert("Login successfully");
     window.location = "../logins1/me75f1f49c-0211-4337-a364-b7602ec38ea3/"; // Redirecting to other page.
     return false;
-  } else if (username == "Corbyn" && password == "2535") {
+  } else if (username == "Corbyn" && password == "MjUzNQ==") {
     alert("Login successfully");
     window.location = "../logins1/cp08d8420f-0eb5-4caf-b841-ce3b5fe68ce4"; // Redirecting to other page.
     return false;
-  } else if (username == "Cale" && password == "2508") {
+  } else if (username == "Cale" && password == "MjUwOA==) {
     alert("Login successfully");
     window.location = "../logins1/cj8e84061b-83d5-4c33-a499-c2118f22686c"; // Redirecting to other page.
     return false;
-  } else if (username == "bp" && password == "poopeater331") {
+  } else if (username == "bp" && password == "cG9vcGVhdGVyMzMx") {
     alert("Login successfully");
     window.location = "../logins1/bp8e84061b-83d5-4c33-a499-c2118f22686c"; // Redirecting to other page.
     return false;
